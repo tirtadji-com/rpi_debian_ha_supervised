8a74041c07742987cef44a42986a38dbffd636e5
Richard Tirtadji <richardtirtadji@Richards-MacBook-Pro.local>
2021-01-10 18:59:56 +0700

    add interval for watchttower for insanity

--- docker/watchtower-install.sh
+++ docker/watchtower-install.sh
@@ -4,7 +4,7 @@
 #   Auto installer for Raspberry on Debian 10 + HA Supervised  
 #   Install Watchtower Docker
 ###############################################################
-# Installation watchtower
-docker run --name="watchtower" -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+# Installation watchtower with interval 7200 seconds
+docker run --name="watchtower" -d --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 7200
 
 echo -e "Watchtower installed \e[32m[DONE]\033[0m"
