429510430dddc6b338be93fe653d595be96dd0d7
Richard Tiirtadji <richardtiirtadji@Richards-MBP-Small.local>
2021-09-09 15:30:34 +0700

    Fixed scripts

--- install/docker-install.sh
+++ install/docker-install.sh
@@ -4,13 +4,16 @@
 #   Auto installer for Raspberry on Debian 11 + HA Supervised  
 #  Install Docker
 ###############################################################
-## Manual install docker latest
-apt-get -y install curl gnupg2 ca-certificates apt-transport-https
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Install Docker
+apt-get -y install docker-ce=5:20.10.8~3-0~debian-bullseye docker-ce-cli=5:20.10.8~3-0~debian-bullseye containerd.io=1.4.9-1
 
-echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get -y update
-apt-get -y install docker-ce docker-ce-cli containerd.io
+## Manual install docker latest
+# apt-get -y install curl gnupg2 ca-certificates apt-transport-https
+# curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt-get -y update
+# apt-get -y install docker-ce docker-ce-cli containerd.io
 
 ## Begin Docker Installation
 #curl -fsSL get.docker.com | sh
