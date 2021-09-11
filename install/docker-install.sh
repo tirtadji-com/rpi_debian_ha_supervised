949c491b48fb5cdbf8ada42a08cb910f0f3f4eb1
Richard Tiirtadji <richardtiirtadji@Richards-MBP-Small.local>
2021-09-11 19:51:54 +0700

    Change installation flow

--- install/docker-install.sh
+++ install/docker-install.sh
@@ -8,7 +8,7 @@
 #apt-get -y install docker-ce=5:20.10.8~3-0~debian-bullseye docker-ce-cli=5:20.10.8~3-0~debian-bullseye containerd.io=1.4.9-1
 
 ## Manual install docker latest
-apt-get -y install curl gnupg2 ca-certificates apt-transport-https
+apt-get -y install curl gnupg ca-certificates apt-transport-https lsb-release
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
